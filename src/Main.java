b2cc102cc5057a03fe9fec153cd63e4b81266607
Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2022-12-03 18:54:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 4000;
+            System.out.println("Цена билета состовляет: " + ticket + " руб.");
+        int miles;
+        if (  ticket >= 20) {
+            miles = ticket / 20;
+            System.out.println("Количество полученный миль будет: " + miles);
+        } else {
+            System.out.println("Количество полученный миль будет: 0");
+        }
+    }
+}
